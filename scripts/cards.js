7c7f5750c0e8c2bcf6cafa3d19ff729e680012cf
Davvos11 <vosdavid2@gmail.com>
2020-11-08 23:54:20 +0100

    added buttons

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,6 +1,6 @@
 import projects from "/projects.js"
 
-function generateCard(title, text, url, imgUrl) {
+function generateCard(title, text, url, imgUrl, buttons) {
     let column = document.createElement('div')
     column.className = "col-md-4 mb-5"
 
@@ -9,7 +9,7 @@ function generateCard(title, text, url, imgUrl) {
     card.className = "card h-100"
 
     let img = document.createElement('div')
-    img.className = `card-img-top ${randomBackgroundClass()}`
+    img.className = `card-img-top ${randomColourClass('bg')}`
     if (imgUrl !== "" && imgUrl !== undefined) {
         let imgImg = document.createElement('img')
         imgImg.src = imgUrl
@@ -37,6 +37,33 @@ function generateCard(title, text, url, imgUrl) {
     card.appendChild(body)
     // </card body>
 
+    // <card footer>
+    let footer = document.createElement('div')
+    footer.className = "card-footer"
+
+    for (let i in buttons) {
+        if (!buttons.hasOwnProperty(i)) continue
+
+        let button = buttons[i]
+        let buttonElement = document.createElement('a')
+        buttonElement.className = `btn ${randomColourClass('btn')}`
+        buttonElement.text = button['text']
+        buttonElement.href = button['url']
+
+        footer.appendChild(buttonElement)
+    }
+
+    if (buttons === undefined || buttons.length === 0) {
+        // If no links are present, generate a hidden button
+        let button = document.createElement('a')
+        button.className = 'btn d-hidden'
+        button.text = 'Hiddem Sound System'
+        footer.appendChild(button)
+    }
+
+    card.appendChild(footer)
+    // </footer>
+
     column.appendChild(card)
     // </card>
 
@@ -51,13 +78,13 @@ function generateCards(projects) {
 
         let project = projects[i]
         cards.append(generateCard(project['title'], project['description'],
-            project['url'], project['imgUrl']))
+            project['url'], project['imgUrl'], project['buttons']))
     }
 }
 
-function randomBackgroundClass() {
+function randomColourClass(bootstrapType) {
     const classes = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'dark']
-        .map(e => `bg-${e}`)
+        .map(e => `${bootstrapType}-${e}`)
     return classes[randInt(0, classes.length)]
 }
 
