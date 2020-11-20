ced9b1c51d0cd75d205e8bb8f50ed09065e5df4d
Davvos11 <vosdavid2@gmail.com>
2020-11-20 16:55:43 +0100

    added share.dovatvis.nl

--- projects.js
+++ projects.js
@@ -4,7 +4,7 @@ export default
     description: 'Website om te kijken of het al tijd is voor een bakkie yoghurt',
     url: 'https://ishetaltijdvooreenbakkieyoghurt.nl',
     buttons: [{text: 'Github', url: 'https://github.com/Davvos11/ishetaltijdvooreenbakkieyoghurt'}]},
-    {title: 'haha dit is een test',
-    description: 'Pils? Ja lekker',
-    url: 'https://www.youtube.com/watch?v=9DIywn76L-U'}
+    {title: 'Share',
+    description: 'Get a temporary URL for uploaded files',
+    url: 'https://share.dovatvis.nl'}
 ]
